3edaa7e1350a0d18c1308de89bf62b0b6eb8a1a0
Jake Callaghan <jake.e.callaghan@gmail.com>
2016-05-15 15:06:56 +0100

    restructered dir

--- ppc-jake 2/peepopt.mli
+++
@@ -1,7 +0,0 @@
-(* ppc/peepopt.mli *)
-
-(* |optimise| -- rewrite list of instructions *)
-val optimise : Keiko.code -> Keiko.code
-
-(* |debug| -- debugging level *)
-val debug: int ref
--- ppc-jake 2/source.mli
+++
@@ -1,9 +0,0 @@
-(* common/source.mli *)
-
-val note_line : int -> Lexing.lexbuf -> unit
-
-val get_line : int -> string
-
-val init : string -> in_channel -> unit
-
-val err_message : string -> Print.arg list -> int -> unit
